033bdcbb7d06d2ece6c62ac2bd193b66b55faf5e
LuisB <Lbrana@miumg.edu.gt>
2021-08-13 17:47:23 -0600

    Se agrega el proyecto

---
+++ Grados.py
@@ -0,0 +1,8 @@
+def fahrenheit_celsius (f):
+    """
+    convertir grados fahrenheit en grados celsius
+    """
+
+    return (f-32)*5.0/9.0
+
+print(fahrenheit_celsius(75))
---
+++ Promedios.py
@@ -0,0 +1,13 @@
+n1 = float(input("ingrese la primera nota"))
+
+n2 = float(input("ingrese la segunda nota"))
+
+n3 = float(input("ingrese la tercera nota"))
+prom = (n1+n2+n3)/3
+
+if(prom>=3):
+    print("su promedio es: ",prom," Felicidades Aprobo")
+   
+else:
+
+    print("su promedio es: ",prom," Lo siento reprobo")
